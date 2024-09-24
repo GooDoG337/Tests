c4e9f4bb6146fb211c96c6b17f9e50fed3be831d
Bloxin <maxretro4rever@gmail.com>
2024-09-24 22:23:23 +0400

    add cin to Coords

--- Coords.cpp
+++ Coords.cpp
@@ -4,9 +4,14 @@
 
 int main()
 {
-	std::vector<std::pair<std::pair<int,int>,double>> coords { {{1,2}, 0}, {{1,5}, 0}, {{50,1}, 0}, {{0,0}, 0}, {{1,1}, 0}, {{-1,-1}, 0}, {{0,10}, 0} };
-	for(size_t x = 0; x < coords.size(); x++)
+	std::vector<std::pair<std::pair<int,int>,double>> coords { };
+	int size;
+	std::cin >> size;
+	for(size_t x = 0; x < size; x++)
 	{
+		coords.push_back( {} );;
+		std::cin >> coords[x].first.first;
+		std::cin >> coords[x].first.second;
 		coords[x].second = coords[x].first.first*coords[x].first.first + coords[x].first.second*coords[x].first.second;
 	}
 	std::sort(coords.begin(), coords.end(), [](const std::pair<std::pair<int,int>, double>& lhs, const std::pair<std::pair<int,int>, double>&  rhs) {return lhs.second < rhs.second;});
